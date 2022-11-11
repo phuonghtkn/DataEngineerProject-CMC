bd85e204e1d4b4f7ef85deb4bfbf41151528c695
phuonghtkn <phuonghtkn@gmail.com>
2022-11-11 11:27:24 +0700

    add mongoDB

---
+++ setup/7MongoDB.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source DE1/bin/activate
+
+sudo apt update
+sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+echo "deb http://security.ubuntu.com/ubuntu focal-security main" | sudo tee /etc/apt/sources.list.d/focal-security.list
+sudo apt update
+sudo apt install libssl1.1
+sudo apt install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
