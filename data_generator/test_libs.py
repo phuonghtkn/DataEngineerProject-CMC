0bd7e4b7d2f7c0cdf78d3e180b93e9038e2f4dea
phuonghtkn <phuonghtkn@gmail.com>
2023-01-11 15:59:47 +0700

    update test file

--- data_generator/test_libs.py
+++ data_generator/test_libs.py
@@ -1,5 +1,17 @@
 from libs.generator import FakeAccountGenerator, FakeCustomerGenerator, FakeTransactionGenerator
 
+
+customer = FakeCustomerGenerator()
+
+list = customer.create_data(1000)
+
+print(list[0])
+print(list[1].head())
+customer.to_file()
+
+customer.stop_spark()
+
+
 account = FakeAccountGenerator()
 
 list = account.create_data(1000)
@@ -11,16 +23,6 @@ print(account.checkBankCustomerInitStatus())
 
 account.stop_spark()
 
-
-customer = FakeCustomerGenerator()
-
-list = customer.create_data(1000)
-
-print(list[0])
-print(list[1].head())
-customer.to_file()
-
-customer.stop_spark()
 
 transaction = FakeTransactionGenerator()
 
